b4bf4877cb7c3b16698d7bc8c64883c978e70bf4
Maksim Kautarov <just5py@yandex.ru>
2023-03-05 21:41:00 +0300

    settings.py changed for staticfiles

--- sipi_back/sipi_back/settings.py
+++ sipi_back/sipi_back/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv("DEBUG_STATUS", 'False').lower() in ('true', '1', 't')
 
 if DEBUG is False:
-    ALLOWED_HOSTS = ['127.0.0.1', 'assistant.5pwjust.ru', '217.151.229.86']
+    ALLOWED_HOSTS = ['127.0.0.1', 'assistant.5pwjust.ru']
 
 
 # Application definition
@@ -146,8 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+if DEBUG is False:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

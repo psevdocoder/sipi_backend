47befff6e7245d739923683035902dd909d78bdc
Maksim Kautarov <just5py@yandex.ru>
2023-03-15 19:33:04 +0300

    start.sh integration 4

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python manage.py migrate --noinput && python manage.py collectstatic --noinput && gunicorn -w 2 -b 0.0.0.0:8000 sipi_back.wsgi:application
+python manage.py migrate --noinput && \
+python manage.py collectstatic --noinput && \
+gunicorn -w 2 -b 0.0.0.0:8000 sipi_back.wsgi:application

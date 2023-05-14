0336550a044e75b221768d5a3a69a0555069bfcd
Maksim <just5py@yandex.ru>
2023-05-14 23:33:31 +0300

    static url

--- sipi_back/sipi_back/settings.py
+++ sipi_back/sipi_back/settings.py
@@ -153,9 +153,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static_backend/'
 if DEBUG is False:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = BASE_DIR / 'static_backend'
 
 
 # Default primary key field type
